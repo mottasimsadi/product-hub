5f6e6c56aabc96eb0c93fbe64e8c6fc99e7e94c3
mottasimsadi <mottasimsadi@gmail.com>
2025-08-23 03:37:47 +0600

    Updated the providers

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from "next-themes";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 
-export function Providers({ children, ...props }: ThemeProviderProps) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider {...props}>
-      <SessionProvider>{children}</SessionProvider>
-    </NextThemesProvider>
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
